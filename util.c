4dc3f4ee9a8686b00240ee1d257a5aa4f5cc4bef
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-05 13:49:37 +0100

    Small fixes

--- util.c
+++ util.c
@@ -42,12 +42,12 @@ util_quantized_low_pass_t util_init_quantizized_low_pass(uint8_t steps,
 }
 
 uint8_t util_add_new_measurent(util_quantized_low_pass_t *quantizized_low_pass, uint16_t measurement) {
-    uint16_t val =
+    quantizized_low_pass->_mem =
             (uint16_t)((uint32_t)quantizized_low_pass->_mem * quantizized_low_pass->low_pass_weight + measurement)/
                     (quantizized_low_pass->low_pass_weight + 1);
     uint16_t step_size = quantizized_low_pass->max_val / quantizized_low_pass->steps;
-    uint8_t raw_step = (uint8_t) (val / step_size);
-    uint16_t delta_to_middle = abs(raw_step * step_size + step_size/2 - val);
+    uint8_t raw_step = (uint8_t) (quantizized_low_pass->_mem / step_size);
+    uint16_t delta_to_middle = abs(raw_step * step_size + step_size/2 - quantizized_low_pass->_mem);
     if (delta_to_middle < step_size / 4) {
         quantizized_low_pass->val = raw_step;
     }
